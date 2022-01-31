b1d910a5480ae5ec3316860f16b1e4c911104b63
Mayank Parashar <92285900+Parasharpy@users.noreply.github.com>
2022-02-01 01:24:34 +0530

    Create Abstract2.java

---
+++ Abstraction/Abstract2.java
@@ -0,0 +1,65 @@
+package oops;
+
+abstract class Writer //abstract class
+{
+  //abstract methods
+	public abstract void write();
+	public abstract void erase();
+}
+
+class Pen extends Writer
+{
+  //abstract methods of abstract class need to be present inside concrete class which extends it
+  public void write()
+	{
+		System.out.println("I am a pen");
+	}
+	
+	public void erase()
+	{
+		System.out.println("I am unerasable");
+	}
+}
+
+class Pencil extends Writer
+{
+	//abstract methods of abstract class need to be present inside concrete class which extends it
+  public void write()
+	{
+		System.out.println("I am a pencil");
+	}
+	
+	public void erase()
+	{
+		System.out.println("I am erasable");
+	}
+}
+
+class Kit
+{
+	public void dosomething(Writer ref) //reference to abstract class which holds some common functionality
+	{
+		ref.write();
+		ref.erase();
+	}
+}
+
+public class AbstractNewDemo {
+
+	public static void main(String[] args) 
+	{
+		Kit obj = new Kit();
+		Writer p = new Pen();
+		Writer pc = new Pencil();
+		
+		obj.dosomething(p);
+		obj.dosomething(pc);
+	}
+
+}
+
+
+//output: I am a pen
+//        I am unerasable
+//        I am a pencil
+//        I am erasable
